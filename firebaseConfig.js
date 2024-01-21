f4d17aa6dad7f9ea4c5a29318887affcb5d29dbd
Jack Lingle <captainjack0818@gmail.com>
2024-01-21 11:02:00 -0500

    initialize firebase app part

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDTnIvbU4iAZg_Axr1lVJzJ_46X-kCKjl8",
+    authDomain: "who-s-here-682f8.firebaseapp.com",
+    projectId: "who-s-here-682f8",
+    storageBucket: "who-s-here-682f8.appspot.com",
+    messagingSenderId: "280308048398",
+    appId: "1:280308048398:web:c6e5078b0d13fdc8ffd25f",
+    measurementId: "G-FZTJQ4ZZMR"
+};
+
+const app = initializeApp(firebaseConfig);
